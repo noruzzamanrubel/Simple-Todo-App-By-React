530faf7ed16615e8ae0ce29faf2ef4d86a70944f
noruzzamanrubel <noruzzamanrubel@gmail.com>
2022-05-07 15:49:49 +0600

    update

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,54 +1,54 @@
 import React, { useState } from "react";
 
 const NewTodo = (props) => {
-  const [todo, setTodo] = useState({
-    title: "",
-    desc: "",
-  });
-
-  const { title, desc } = todo;
-
-  const handleChange = (e) => {
-    const name = e.target.name;
-    setTodo((oldTodo) => {
-      return {
-        ...oldTodo,
-        [name]: e.target.value,
-      };
+    const [todo, setTodo] = useState({
+        title: "",
+        desc: "",
     });
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.onAddTodo(todo);
-    setTodo({ title: "", desc: "" });
-  };
+    const { title, desc } = todo;
 
-  return (
-    <form className="form" onSubmit={handleSubmit}>
-      <div className="form-field">
-        <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          value={title}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-field">
-        <label htmlFor="desc">Description</label>
-        <textarea
-          type="text"
-          id="desc"
-          name="desc"
-          value={desc}
-          onChange={handleChange}
-        />
-      </div>
-      <button type="submit">Add Todo</button>
-    </form>
-  );
+    const handleChange = (e) => {
+        const name = e.target.name;
+        setTodo((oldTodo) => {
+            return {
+                ...oldTodo,
+                [name]: e.target.value,
+            };
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.onAddTodo(todo);
+        setTodo({ title: "", desc: "" });
+    };
+
+    return (
+        <form className="form" onSubmit={handleSubmit}>
+            <div className="form-field">
+                <label htmlFor="title">Title</label>
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    value={title}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-field">
+                <label htmlFor="desc">Description</label>
+                <textarea
+                    type="text"
+                    id="desc"
+                    name="desc"
+                    value={desc}
+                    onChange={handleChange}
+                />
+            </div>
+            <button type="submit">Add Todo</button>
+        </form>
+    );
 };
 
 export default NewTodo;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,18 +3,18 @@ import { v4 as uuidv4 } from "uuid";
 import Todo from "./Todo";
 
 const Todos = (props) => {
-  return (
-    <section className="todos">
-      {props.todos.map((todo) => (
-        <Todo
-          key={uuidv4()}
-          todo={todo}
-          id={todo.id}
-          onRemoveTodo={props.onRemoveTodo}
-        />
-      ))}
-    </section>
-  );
+    return (
+        <section className="todos">
+            {props.todos.map((todo) => (
+                <Todo
+                    key={uuidv4()}
+                    todo={todo}
+                    id={todo.id}
+                    onRemoveTodo={props.onRemoveTodo}
+                />
+            ))}
+        </section>
+    );
 };
 
 export default Todos;
